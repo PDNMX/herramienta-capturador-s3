f5603e38e58cfe5612dfe5a59e58a7437499b12a
Héctor Franco <hhernandezf1800@alumno.ipn.mx>
2024-02-26 11:26:54 -0600

    creacion de endpoint s3ang funcionando

---
+++ backend/api/sistema3/models/S3ang.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const s3angSchema = new mongoose.Schema({
+     meta: mongoose.Mixed,
+
+}, { "strict": false });
+
+s3angSchema.plugin(mongoosePaginate);
+const s3angConnection = mongoose.connection.useDb("administracionUsuarios");
+const S3ang = mongoose.connection.useDb('S3').model("s3ang", s3angSchema, "s3ang");
+//const s3ag = mongoose.model("s3ag", s3agSchema, "s3ag");
+
+module.exports = {s3angConnection, S3ang}; 
--- backend/api/sistema3/routes.js
+++ backend/api/sistema3/routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 // Controller Imports
 const S3_Controller_s3ag = require("./controllers/S3_Controller_s3ag");
+const S3_Controller_s3ang = require("./controllers/S3_Controller_s3ang");
 //const ProviderController = require("./controllers/ProviderController");
 // Middleware Imports
 const isAuthenticatedMiddleware = require("./../common/middlewares/IsAuthenticatedMiddleware");
@@ -11,7 +12,7 @@ async (req, res) => {
     res.status(200).send("Sistema 3 AG funcionando correctamente. Ahora a probar los endpoints");
     }
 );
-
+//rutas ag
 router.post("/insert_s3ag", 
             [isAuthenticatedMiddleware.check,],
             S3_Controller_s3ag.create_s3ag);
@@ -24,11 +25,19 @@ router.post("/list_s3ag",
 router.put("/update_s3ag",
             [isAuthenticatedMiddleware.check,],
             S3_Controller_s3ag.update_s3ag);
+//rutas ang
+router.post("/insert_s3ang",
+            [isAuthenticatedMiddleware.check,],
+            S3_Controller_s3ang.create_S3ang);
             
-/*router.post("/edit_s3ag",[isAuthenticatedMiddleware.check,],S3_Controller_s3ag.edit_s3ag);
-router.post("/get_s3ag",[isAuthenticatedMiddleware.check,],S3_Controller_s3ag.get_s3ag);
-router.post("/get_all_s3ag",[isAuthenticatedMiddleware.check,],S3_Controller_s3ag.get_all_s3ag);
-router.post("/delete_s3ag",[isAuthenticatedMiddleware.check,],S3_Controller_s3ag.delete_s3ag);
-router.post("/update_s3ag",[isAuthenticatedMiddleware.check,],S3_Controller_s3ag.update_s3ag); */
-
+router.post("/getAll_s3ang",
+            [isAuthenticatedMiddleware.check,],
+            S3_Controller_s3ang.getAll_S3ang);
+router.post("/list_s3ang",
+            [isAuthenticatedMiddleware.check,],
+            S3_Controller_s3ang.list_S3ang);
+router.put("/update_s3ang",
+            [isAuthenticatedMiddleware.check,],
+            S3_Controller_s3ang.update_S3ang);
+            
 module.exports = router;
