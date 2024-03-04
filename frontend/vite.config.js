cf9889cff491756dd89b60126bd0a8e292c62160
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-03-04 16:27:28 -0600

    change api (dir api)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    VITE_URL_API: process.env.VITE_URL_API,
+    VITE_URL_OAUTH: process.env.VITE_URL_OAUTH,
+    VITE_CLIENT_ID: process.env.VITE_CLIENT_ID,
+    VITE_CLIENT_SECRET: process.env.VITE_CLIENT_SECRET,
+  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
