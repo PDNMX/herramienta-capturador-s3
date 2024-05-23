d14c4412549960507d594d4c2c4faf2a76e1b003
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2024-05-23 16:28:02 -0600

    cambiando sistemas/permisos al mongoInit

--- mongo-init.sh
+++ mongo-init.sh
@@ -13,7 +13,7 @@ db.usuarios.insertOne({
   extension: '',
   usuario: '$ADMIN_USER',
   contrasena: '$(echo -n "$ADMIN_PASSWORD" | sha256sum | cut -c-64)',
-  sistemas: ['S2', 'S3S', 'S3P'],
+  sistemas: [ "faltas-administrativas.graves", "actos-particulares.personas-morales", "hechos-corrupcion.personas-morales", "abstenciones.graves", "faltas-administrativas.no-graves", "actos-particulares.personas-fisicas", "hechos-corrupcion.servidores-publicos", "hechos-corrupcion.personas-fisicas", "abstenciones.no-graves" ],
   proveedorDatos: 'Proveedor DEMO',
   fechaAlta: '2021-01-15T15:28:28-06:00',
   vigenciaContrasena: '2029-04-15T15:28:28-05:00',
