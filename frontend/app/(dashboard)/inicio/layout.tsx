42946391fdef014107108bf71494efc8fe7edbec
RBalan21 <aln.rojas.pdn@gmail.com>
2026-01-22 12:37:01 -0600

    fix s3, en lugar de cobertura

--- frontend/app/(dashboard)/inicio/layout.tsx
+++ frontend/app/(dashboard)/inicio/layout.tsx
@@ -1,10 +1,11 @@
+//@ts-nocheck
 import Header from "@/components/layout/header";
 import Sidebar from "@/components/layout/sidebar";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: process.env.NEXT_PUBLIC_APP_NAME || "Cobertura",
-  description: "Sistema de Cobertura",
+  title: process.env.NEXT_PUBLIC_APP_NAME || "S3",
+  description: "Sistema 3",
 };
 
 export default function DashboardLayout({
